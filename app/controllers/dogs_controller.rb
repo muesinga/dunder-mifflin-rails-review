cb9351e82069b4f2e251529a01bc4d3f45e8880c
“Aaron <github email address>
2021-02-05 18:32:23 -0500

    commit tree cceb8e1ff67521fa2c035b0fe730fdec4b7128ad

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,21 @@
 class DogsController < ApplicationController
+    
+    def index
+        @dogs = Dog.all
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def new
+        @dog = Dog.new
+    end
+
+    def create
+        @dog = Dog.create(dog_params)
+    end
+
+    def 
+    end
 end
